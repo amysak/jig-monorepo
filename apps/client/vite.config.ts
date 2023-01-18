add8aef502d985582492fddf8b27c22c7dc608a3
Alexander Mysak <reactionnnnn@gmail.com>
2023-01-18 15:29:23 +0200

    Preview port

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -13,7 +13,10 @@ export default defineConfig(({ mode }) => {
   const dirSrc = path.resolve(__dirname, "src").replace(/\\/g, "/");
 
   return {
-    server: { hmr: true, port: +env.PORT || 3000 },
+    server: { hmr: true, port: 3000 },
+    preview: {
+      port: +env.PORT || 4173,
+    },
     envPrefix: "APP_",
     define: {},
     build: {
