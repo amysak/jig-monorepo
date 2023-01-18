3053b6d542044b954ce3c041e4774cbaff99c719
Alexander Mysak <reactionnnnn@gmail.com>
2023-01-18 16:21:06 +0200

    Ignore & fix for deployment

--- packages/type-defs/bin/typegen.sh
+++ packages/type-defs/bin/typegen.sh
@@ -2,16 +2,16 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 cd ..
-echo "Current directory: $(pwd)"
-echo "Directory contents two levels up: $(ls ../../)"
 
 rm -rf dist
 cp -R ../../apps/server/src/database/entities src
 
-echo "Src contents: $(ls src)"
-echo "Find results: $(find ./src/entities -type f -name '*.ts')"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  find src/entities -type f -name '*.ts' -exec sed -i '' -e 's/type-defs/\.\.\/types/g' {} \; 
+else
+  find src/entities -type f -name '*.ts' -exec sed -i -e 's/type-defs/\.\.\/types/g' {} \; 
+fi
 
-find ./src/entities -type f -name '*.ts' -exec sed -i '' -e 's/type-defs/\.\.\/types/g' {} \; 
 
 tsc -p tsconfig.typegen.json
 
